6f9fcd88b8251ce21d70c52829c9a9a00ebd50f4
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-10-25 16:42:19 -0300

    adiciona formato básico da main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "../include/CamadaFisicaTransmissora.hpp"
 #include "../include/Utils.hpp"
 #include "../include/BitArray.hpp"
+#include "../include/Simulacao.hpp"
 
 using namespace std;
 
@@ -12,14 +13,20 @@ int main() {
     string mensagem;
     int tipo_codigo;
 
+    // Recebe a mensagem do usuário
     std::system("clear");
     cout << "Digite a mensagem que deseja transmitir: " << endl << endl;
     getline(cin, mensagem);
 
+    // Recebe a codificação a ser utilizada
     tipo_codigo = printMenu();
 
-    // TODO Selecionar codificação
-    BitArray a = BitArray("a");
-    a.print();
+    // Instancia uma nova simulação
+    Simulacao *simulacao = new Simulacao(tipo_codigo);
+
+    simulacao->camadaDeAplicacaoTransmissora(mensagem);
+
     return 0;
 }
+
+// TODO fazer separadamente a Aplicação Transmissora/Receptora ou deixar como está

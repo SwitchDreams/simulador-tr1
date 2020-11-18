8825f95b8411d1cc52b1ee1d83a355e4c2c0c66d
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-11-18 19:00:45 -0300

    adds enlace defs

---
+++ src/CamadaEnlaceReceptora.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../include/CamadaEnlaceReceptora.hpp"
+
+BitArray* CERContagemCaracteres::execute(BitArray * quadro) {
+    return quadro;
+};
+
+BitArray* CERInsercaoBytes::execute(BitArray * quadro) {
+    return quadro;
+};
+
+BitArray* CERInsercaoBits::execute(BitArray * quadro) {
+    return quadro;
+};
---
+++ src/CamadaEnlaceTransmissora.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../include/CamadaEnlaceTransmissora.hpp"
+
+BitArray* CETContagemCaracteres::execute(BitArray * quadro) {
+    return quadro;
+};
+
+BitArray* CETInsercaoBytes::execute(BitArray * quadro) {
+    return quadro;
+};
+
+BitArray* CETInsercaoBits::execute(BitArray * quadro) {
+    return quadro;
+};

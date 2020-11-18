00260ba831aecafc2d9a32d98a47b3ff02bd981e
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2020-11-18 20:06:25 -0300

    Método de decodificação de cabeçalho

--- src/CamadaEnlaceReceptora.cpp
+++ src/CamadaEnlaceReceptora.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 #include "../include/CamadaEnlaceReceptora.hpp"
 
-BitArray* CERContagemCaracteres::execute(BitArray * quadro) {
+BitArray *CERContagemCaracteres::execute(BitArray *quadro) {
+    std::cout << "Decodificação Contagem de Caracteres: ";
+
+    // Tamanho do quadro em BYTES
+    int tamanhoQuadro = (int) quadro->getCabecalho();
+
+    // Cria um novo quadro sem o cabeçalho
+    auto *quadroSemCabecalho = new BitArray(tamanhoQuadro * BYTE_SIZE);
+
+    for (int i = 0; i < tamanhoQuadro * BYTE_SIZE; i++) {
+        // Começa do segundo byte porconta do cabeçalho
+        if ((*quadro)[i + BYTE_SIZE]) {
+            quadroSemCabecalho->setBit(i);
+        } else {
+            quadroSemCabecalho->clearBit(i);
+        }
+
+    }
+
+    quadroSemCabecalho->print();
+    std::cout << std::endl;
+    return quadroSemCabecalho;
+};
+
+BitArray *CERInsercaoBytes::execute(BitArray *quadro) {
     return quadro;
 };
 
-BitArray* CERInsercaoBytes::execute(BitArray * quadro) {
-    return quadro;
-};
-
-BitArray* CERInsercaoBits::execute(BitArray * quadro) {
+BitArray *CERInsercaoBits::execute(BitArray *quadro) {
     return quadro;
 };

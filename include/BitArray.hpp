a2a6e0f8a7ee9a1cd976108525c42dfaf79c61ff
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-11-19 19:35:21 -0300

    adiciona defines de BIT_FLAG e TAM_QUADRO

--- include/BitArray.hpp
+++ include/BitArray.hpp
@@ -6,9 +6,12 @@
 
 #define BYTE_SIZE 8 //!< Tamanho de um byte em bits
 #define BYTE_FLAG (uint8_t) 0b1000000
+#define BIT_FLAG (uint8_t) 0b01111110
 #define BYTE_ESC (uint8_t) 0b00100011
 #define BIT_8 (uint8_t) 0b10000000
 #define BIT_1 (uint8_t) 0x01
+#define TAM_QUADRO BYTE_SIZE
+
 
 /*
  *  Classe BitArray

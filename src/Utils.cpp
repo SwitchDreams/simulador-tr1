d2b23e4d2843d970db3418beee1ca14219706c96
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-11-19 19:34:54 -0300

    Adiciona método para inserir um byte num vector

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -25,3 +25,9 @@ int getMenuEnquadramento() {
     std::cin >> tipo_codigo;
     return tipo_codigo;
 }
+
+void insereBytesComoBits(uint8_t byte, std::vector<uint8_t>* quadro) {
+    for (unsigned int i = 0; i < 8; i++) {
+        quadro->push_back(byte >> i & 0x01);
+    }
+}

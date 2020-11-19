197340ea2a49f87e66c24cf80c02de1e94215ed8
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-11-19 13:25:34 -0300

    Definindo Byte Flag e Byte Esc

--- include/BitArray.hpp
+++ include/BitArray.hpp
@@ -5,6 +5,10 @@
 #include <string>
 
 #define BYTE_SIZE 8 //!< Tamanho de um byte em bits
+#define BYTE_FLAG (uint8_t) 0b1000000
+#define BYTE_ESC (uint8_t) 0b01010101
+#define BIT_8 (uint8_t) 0b10000000
+#define BIT_1 (uint8_t) 0x01
 
 /*
  *  Classe BitArray

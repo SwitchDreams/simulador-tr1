237255c5ea7ae68ac6a6ae8f970ee5299858a16e
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-12-05 18:49:41 -0300

    adds cpp error files

---
+++ src/CamadaReceptoraControleDeErro.cpp
@@ -0,0 +1,17 @@
+#include "../include/CamadaReceptoraControleDeErro.hpp"
+
+BitArray *CRCEParidadePar::execute(BitArray* quadro) {
+    return quadro;
+}
+
+BitArray *CRCEParidadeImpar::execute(BitArray* quadro) {
+    return quadro;
+}
+
+BitArray *CRCECRC::execute(BitArray* quadro) {
+    return quadro;
+}
+
+BitArray *CRCEHamming::execute(BitArray* quadro) {
+    return quadro;
+}
---
+++ src/CamadaTransmissoraControleDeErro.cpp
@@ -0,0 +1,17 @@
+#include "../include/CamadaTransmissoraControleDeErro.hpp"
+
+BitArray *CTCEParidadePar::execute(BitArray* quadro) {
+    return quadro;
+}
+
+BitArray *CTCEParidadeImpar::execute(BitArray* quadro) {
+    return quadro;
+}
+
+BitArray *CTCECRC::execute(BitArray* quadro) {
+    return quadro;
+}
+
+BitArray *CTCEHamming::execute(BitArray* quadro) {
+    return quadro;
+}
